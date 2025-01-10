0132c7ccda9951ce93f5d19a766dca893567b55f
EMG <edgarmg22@gmail.com>
2025-01-10 16:22:44 +0100

    modificar tests

--- cypress/e2e/1-my-tests/api/users.spec.js
+++ cypress/e2e/1-my-tests/api/users.spec.js
@@ -4,12 +4,9 @@ let TOTAL_USERS = 10;
 
 describe("Test de endpoints", () => {
   before(() => {
-    // Esto asegura que la vista está lista antes de realizar peticiones
-    cy.visit("/"); // Asegúrate de que la URL sea accesible y que no haya problemas en el contenedor
-
-    // Realiza la solicitud DELETE para limpiar los usuarios antes de los tests
-    cy.request("DELETE", "/api/users", {}).then((response) => {
-      expect(response.body).to.have.property("message", "All users deleted");
+    cy.visit("/");
+    cy.request("DELETE", "api/users", {}).then((response) => {
+      expect(response.body).to.have.property("message", `All users deleted`);
     });
   });
 
@@ -20,31 +17,26 @@ describe("Test de endpoints", () => {
       const testName = `testname${id}`;
       const testLastName = `testname${id}`;
 
-      // Realiza la solicitud POST para crear un nuevo usuario
-      cy.request("POST", "/api/users", {
+      cy.request("POST", "api/users", {
         name: testName,
         lastName: testLastName,
         email: `${testName}@gmai.com`,
       }).then((response) => {
-        // Comprueba que la respuesta contenga el saludo esperado
         expect(response.body).to.have.property("greeting", `Hello ${testName}`);
       });
     });
   });
 
   it("Endpoint get users", () => {
-    // Realiza la solicitud GET para obtener los usuarios
-    cy.request("GET", "/api/users", {}).then((response) => {
-      // Verifica que la cantidad de usuarios sea la misma que TOTAL_USERS
+    cy.request("GET", "api/users", {}).then((response) => {
       expect(response.body).to.have.lengthOf(TOTAL_USERS);
     });
   });
 
   after(() => {
-    // Limpia la base de datos después de los tests
     cy.visit("/");
-    cy.request("DELETE", "/api/users", {}).then((response) => {
-      expect(response.body).to.have.property("message", "All users deleted");
+    cy.request("DELETE", "api/users", {}).then((response) => {
+      expect(response.body).to.have.property("message", `All users deleted`);
     });
   });
 });
