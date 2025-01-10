c2acdd99eb117f2da9f1644f5a474322def635de
EMG <edgarmg22@gmail.com>
2025-01-10 16:04:12 +0100

    modificar tests

--- cypress/e2e/1-my-tests/api/users.spec.js
+++ cypress/e2e/1-my-tests/api/users.spec.js
@@ -4,8 +4,11 @@ let TOTAL_USERS = 10;
 
 describe("Test de endpoints", () => {
   before(() => {
-    cy.visit("/");
-    cy.request("DELETE", "api/users", {}).then((response) => {
+    // Esto asegura que la vista está lista antes de realizar peticiones
+    cy.visit("/"); // Asegúrate de que la URL sea accesible y que no haya problemas en el contenedor
+
+    // Realiza la solicitud DELETE para limpiar los usuarios antes de los tests
+    cy.request("DELETE", "/api/users", {}).then((response) => {
       expect(response.body).to.have.property("message", "All users deleted");
     });
   });
@@ -17,25 +20,30 @@ describe("Test de endpoints", () => {
       const testName = `testname${id}`;
       const testLastName = `testname${id}`;
 
-      cy.request("POST", "api/users", {
+      // Realiza la solicitud POST para crear un nuevo usuario
+      cy.request("POST", "/api/users", {
         name: testName,
         lastName: testLastName,
         email: `${testName}@gmai.com`,
       }).then((response) => {
+        // Comprueba que la respuesta contenga el saludo esperado
         expect(response.body).to.have.property("greeting", `Hello ${testName}`);
       });
     });
   });
 
   it("Endpoint get users", () => {
-    cy.request("GET", "api/users", {}).then((response) => {
+    // Realiza la solicitud GET para obtener los usuarios
+    cy.request("GET", "/api/users", {}).then((response) => {
+      // Verifica que la cantidad de usuarios sea la misma que TOTAL_USERS
       expect(response.body).to.have.lengthOf(TOTAL_USERS);
     });
   });
 
   after(() => {
+    // Limpia la base de datos después de los tests
     cy.visit("/");
-    cy.request("DELETE", "api/users", {}).then((response) => {
+    cy.request("DELETE", "/api/users", {}).then((response) => {
       expect(response.body).to.have.property("message", "All users deleted");
     });
   });
